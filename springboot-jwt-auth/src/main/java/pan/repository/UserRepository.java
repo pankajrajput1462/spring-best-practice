b53eea9bf430dfb12b5385e96e2702d17efbd05e
pankajrajput1462 <pankajrajput1462@gmail.com>
2019-08-01 00:03:14 +0530

    Boot jwt sample

---
+++ springboot-jwt-auth/src/main/java/pan/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package pan.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pan.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  boolean existsByUsername(String username);
+
+  User findByUsername(String username);
+
+  @Transactional
+  void deleteByUsername(String username);
+
+}

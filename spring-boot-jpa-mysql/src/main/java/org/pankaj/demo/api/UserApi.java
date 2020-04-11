d9e4d1f7ccf2590d783e828e326f8ae568dd8500
pankaj <pankaj@epoca-sante.fr>
2020-04-11 15:09:06 +0200

    Added Post model

---
+++ spring-boot-jpa-mysql/src/main/java/org/pankaj/demo/api/UserApi.java
@@ -0,0 +1,49 @@
+package org.pankaj.demo.api;
+
+import org.pankaj.demo.model.User;
+import org.pankaj.demo.model.leader.Address;
+import org.pankaj.demo.repo.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserApi {
+
+    private final UserRepository userRepository;
+
+    public UserApi(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/user/{userName}")
+    public User showSignUpForm(@PathVariable String userName) {
+        return userRepository.findByName(userName);
+    }
+
+    @PostMapping("/user")
+    public User addUser() {
+        final User user = new User();
+        user.setEmail("pankaj@gmail.com");
+        user.setName("Pankaj");
+        List<Address> addressList = new ArrayList<>();
+        final Address address1 = new Address();
+        address1.setCountry("India");
+        address1.setStreet("Awas Vikas");
+
+        final Address address2 = new Address();
+        address2.setCountry("India");
+        address2.setStreet("Awas Vikas");
+
+        addressList.add(address1);
+        addressList.add(address2);
+//        user.setAddresses(addressList);
+
+        return userRepository.save(user);
+    }
+
+}
---
+++ spring-boot-jpa-mysql/src/main/java/org/pankaj/demo/model/User.java
@@ -0,0 +1,28 @@
+package org.pankaj.demo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+//    @OneToMany(mappedBy = "address", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    List<Address> addresses;
+}

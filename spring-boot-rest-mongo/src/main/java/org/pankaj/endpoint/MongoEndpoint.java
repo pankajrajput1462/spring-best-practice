7c9d66171ab330612c8dc906bddce2010cd884fe
pankaj <pankaj@epoca-sante.fr>
2020-04-11 03:00:12 +0200

    changed MongoEndpoint and rename project

--- spring-boot-rest-mongo/src/main/java/org/pankaj/endpoint/MongoEndpoint.java
+++ spring-boot-rest-mongo/src/main/java/org/pankaj/endpoint/MongoEndpoint.java
@@ -18,42 +18,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MongoEndpoint {
 
-	@Autowired
-	private CustomerRepository customerRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
 
-	@GetMapping(path = "/getAllCustomer", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<List<Customer>> getCustomerData() {
-		List<Customer> findByEmployeeName = customerRepository.findAll();
-		return new ResponseEntity<>(findByEmployeeName, HttpStatus.OK);
+    @GetMapping(path = "/getAllCustomer", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Customer>> getCustomerData() {
+        List<Customer> findByEmployeeName = customerRepository.findAll();
+        return new ResponseEntity<>(findByEmployeeName, HttpStatus.OK);
 
-	}
-	
-	@GetMapping(path = "/getAllCustomerByName/{firstName}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<List<Customer>> getCustomerDataByName(@PathVariable("firstName") String firstName) {
-		List<Customer> findByEmployeeName = customerRepository.findByEmployeeName(firstName);
-		return new ResponseEntity<>(findByEmployeeName, HttpStatus.OK);
+    }
 
-	}
+    @GetMapping(path = "/getAllCustomerByName/{firstName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Customer>> getCustomerDataByName(@PathVariable("firstName") String firstName) {
+        List<Customer> findByEmployeeName = customerRepository.findByEmployeeName(firstName);
+        return new ResponseEntity<>(findByEmployeeName, HttpStatus.OK);
 
-	@GetMapping(path = "/getAllCustomerByLastName/{lastName}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<List<Customer>> getCustomerDataByLastName(@PathVariable("lastName") String lastName) {
-		List<Customer> findByEmployeeLastName = customerRepository.findByEmployeeLastName(lastName);
-		return new ResponseEntity<>(findByEmployeeLastName, HttpStatus.OK);
+    }
 
-	}
+    @GetMapping(path = "/getAllCustomerByLastName/{lastName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Customer>> getCustomerDataByLastName(@PathVariable("lastName") String lastName) {
+        List<Customer> findByEmployeeLastName = customerRepository.findByEmployeeLastName(lastName);
+        return new ResponseEntity<>(findByEmployeeLastName, HttpStatus.OK);
+
+    }
 
 
-	@PostMapping(path = "/saveCustomer", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Customer> saveCustomerData(@RequestBody Customer customer) {
-		Customer save = customerRepository.save(customer);
-		return new ResponseEntity<>(save, HttpStatus.OK);
-	}
+    @PostMapping(path = "/saveCustomer", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> saveCustomerData(@RequestBody Customer customer) {
+        Customer save = customerRepository.save(customer);
+        return new ResponseEntity<>(save, HttpStatus.OK);
+    }
 
-	@DeleteMapping(path = "/deleteCustomer/{firstName}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public ResponseEntity<Customer> deleteCustomerData(@PathVariable("firstName") String firstName) {
-		List<Customer> findByEmployeeName = customerRepository.findByEmployeeName(firstName);
-		Customer customer = findByEmployeeName.stream().findFirst().get();
-		customerRepository.delete(customer);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+    @DeleteMapping(path = "/deleteCustomer/{firstName}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> deleteCustomerData(@PathVariable("firstName") String firstName) {
+        List<Customer> findByEmployeeName = customerRepository.findByEmployeeName(firstName);
+        Customer customer = findByEmployeeName.stream().findFirst().get();
+        customerRepository.delete(customer);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
